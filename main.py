d1156af8eaf0e765a2d9d264c92f00bd6dd5de14
Eniola Oladejo <enioladejo1725@gmail.com>
2025-08-16 02:55:30 +0100

    updated

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
 
 
-def get_input(init=[]):
- line = input()
- if line:
-     return get_input(init + [line])
- return init
+def get_input(count=None, init=()):
+ if count is None:
+  return init
+ return init if count <= 0 else get_input(count - 1, init + (input().strip(),))
 
 
 def check_match(lines, index):
@@ -13,7 +12,7 @@ def check_match(lines, index):
  prev = lines[index - 1]
  nums = list(map(int, current.split()))
  if len(nums) > 1 :
-     return len(nums) == int(prev)
+  return len(nums) == int(prev)
  return None
 
 
@@ -24,16 +23,17 @@ def calculate(line):
 
 
 def process_line(lines, index):
-    match_result = check_match(lines, index)
-    if match_result is not None: 
-        return calculate(lines[index]) if match_result else -1
-    return None
+ match_result = check_match(lines, index)
+ if match_result is not None: 
+  return calculate(lines[index]) if match_result else -1
+ return None
 
 def main():
- data = get_input()
- indexes = range(1, len(data))
+ t = int(input().strip())
+ data = get_input(count=2*t)
+ indexes = range(1, len(data), 2)
  results = filter(lambda x: x is not None, map(lambda i: process_line(data, i), indexes))
- print('\n'.join(map(str, results)))
+ sys.stdout.write("\n".join(map(str, results)))
 
 if __name__ == "__main__":
-    main()
+ main()
